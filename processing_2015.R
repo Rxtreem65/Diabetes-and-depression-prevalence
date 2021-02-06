bb1f89d43e264471bcc170bf4b4449a32c251a16
Rxtreem65 <52712304+Rxtreem65@users.noreply.github.com>
2021-02-06 15:02:09 +0530

    Update processing_2015.R

--- processing_2015.R
+++ processing_2015.R
@@ -1,6 +1,7 @@
 library(SASxport)
 
-data2015 <- read.xport("C:/Users/riyap/Downloads/data/LLCP2015.XPT")
+data2015 <- read.xport("LLCP2015.XPT")
+#features to include in the model
 fea <- c("DIABAGE2","X.AGEG5YR","SEX","X.RACE.G1","X.BMI5","EXERANY2",
          "MENTHLTH","INCOME2","EMPLOY1","EDUCA","SCNTMNY1","SCNTMEL1",
          "RENTHOM1","DIABETE3","ADDEPEV2")
@@ -21,10 +22,10 @@ std_dia = sd(data2015$DIABAGE2) #standard deviation of diabetes age
 std_dia
 mdl = mean_dia-1.96*std_dia #lower interval of CI
 mdh = mean_dia+1.96*std_dia #upper interval of CI
-mdl
-mdh
+mdl #lower interval
+mdh #upper interval
 #CI 95% subset
-data2015 = subset(data2015, data2015$DIABAGE2 >= 30) 
+data2015 = subset(data2015, data2015$DIABAGE2 >= 30) #diabetes diagnose age of 30 or above
 data2015 = subset(data2015, data2015$DIABAGE2 <= mdh)
 table(data2015$DIABAGE2)
 nrow(data2015)
@@ -32,26 +33,26 @@ nrow(data2015)
 
 #Processing Age group
 table(data2015$X.AGEG5YR)
-data2015 = data2015[!is.na(data2015$X.AGEG5YR),]
-data2015 = subset(data2015, data2015$X.AGEG5YR<14)
+data2015 = data2015[!is.na(data2015$X.AGEG5YR),] #removing missing values
+data2015 = subset(data2015, data2015$X.AGEG5YR<14) #include 1 to 13 age groups
 
 
 #processing for gender
 table(data2015$SEX)
-data2015 = data2015[!is.na(data2015$SEX),]
+data2015 = data2015[!is.na(data2015$SEX),] #removing missing values
 table(data2015$SEX)
 nrow(data2015)
 
 #processing for ethnicity 
 table(data2015$X.RACE.G1)
-data2015 = data2015[!is.na(data2015$X.RACE.G1),]
-data2015 = subset(data2015, data2015$X.RACE.G1<6)
+data2015 = data2015[!is.na(data2015$X.RACE.G1),] #removing missing values
+data2015 = subset(data2015, data2015$X.RACE.G1<6) #include 1 to 5 sub groups
 table(data2015$X.RACE.G1)
 nrow(data2015)
 
 #processing for BMI
 summary(data2015$X.BMI5)
-data2015 = data2015[!is.na(data2015$X.BMI5),]#removing missing values
+data2015 = data2015[!is.na(data2015$X.BMI5),] #removing missing values
 hist(data2015$X.BMI5, main = "Histogram of BMI of respondents",
      xlabe = "BMI",
      ylabe = "number of respondents") #histogram plot for BMI
@@ -62,8 +63,8 @@ std_b = sd(data2015$X.BMI5) #standard deviation of bmi
 std_b
 mbl = mean_b-1.96*std_b #lower interval of CI
 mbh = mean_b+1.96*std_b #upper interval of CI
-mbl
-mbh
+mbl #lower value
+mbh #upper value
 #CI 95% subset
 data2015 = subset(data2015, data2015$X.BMI5 >= mbl) 
 data2015 = subset(data2015, data2015$X.BMI5 <= mbh)
@@ -73,93 +74,77 @@ nrow(data2015)
 
 #processing for exercise
 table(data2015$EXERANY2)
-data2015 = data2015[!is.na(data2015$EXERANY2),]
-data2015 = subset(data2015, data2015$EXERANY2<7)
+data2015 = data2015[!is.na(data2015$EXERANY2),] #removing missing values
+data2015 = subset(data2015, data2015$EXERANY2<7) #exercise (1) yes or (2) no
 table(data2015$EXERANY2)
 nrow(data2015)
 
 #processing for mental health
 table(data2015$MENTHLTH)
-data2015 = data2015[!is.na(data2015$MENTHLTH),]
+data2015 = data2015[!is.na(data2015$MENTHLTH),] #removing missing values
 data2015$MENTHLTH[data2015$MENTHLTH == 88] <- 0
-data2015 = subset(data2015, data2015$MENTHLTH<77)
+data2015 = subset(data2015, data2015$MENTHLTH<77) #mental health not good 0 to 30 days
 table(data2015$MENTHLTH)
 nrow(data2015)
 
 #processing for income
 table(data2015$INCOME2)
-data2015 = data2015[!is.na(data2015$INCOME2),]
-data2015 = subset(data2015, data2015$INCOME2<9)
+data2015 = data2015[!is.na(data2015$INCOME2),] #removing missing values
+data2015 = subset(data2015, data2015$INCOME2<9) #income levels 1 to 8
 table(data2015$INCOME2)
 nrow(data2015)
 
 #processing for employment
 table(data2015$EMPLOY1)
-data2015 = data2015[!is.na(data2015$EMPLOY1),]
+data2015 = data2015[!is.na(data2015$EMPLOY1),] #removing missing values
+#arranging the level of employment status
 data2015 <- replace(data2015, data2015==3,0)
 data2015 <- replace(data2015, data2015==5,3)
 data2015 <- replace(data2015, data2015==4,5)
 data2015 <- replace(data2015, data2015==6,4)
 data2015 <- replace(data2015, data2015==0,6)
-data2015 = subset(data2015, data2015$EMPLOY1<9)
+data2015 = subset(data2015, data2015$EMPLOY1<9) #including 1 to 8 levels of employment status
 table(data2015$EMPLOY1)
 nrow(data2015)
 
 #processing for education 
 table(data2015$EDUCA)
-data2015 = data2015[!is.na(data2015$EDUCA),]
-data2015 = subset(data2015, data2015$EDUCA<9)
+data2015 = data2015[!is.na(data2015$EDUCA),] #removing missing values
+data2015 = subset(data2015, data2015$EDUCA<9) #including 1 to 8 education level
 table(data2015$EDUCA)
 nrow(data2015)
 
 #processing  for worried about rent
 table(data2015$SCNTMNY1)
-data2015 = data2015[!is.na(data2015$SCNTMNY1),]
-data2015 = subset(data2015, data2015$SCNTMNY1<6)
+data2015 = data2015[!is.na(data2015$SCNTMNY1),] #removing missing values
+data2015 = subset(data2015, data2015$SCNTMNY1<6) 
 table(data2015$SCNTMNY1)
 nrow(data2015)
 
 #processing for worried about meal
 table(data2015$SCNTMEL1)
-data2015 = data2015[!is.na(data2015$SCNTMEL1),]
+data2015 = data2015[!is.na(data2015$SCNTMEL1),] #removing missing values
 data2015 = subset(data2015, data2015$SCNTMEL1<6)
 table(data2015$SCNTMEL1)
 nrow(data2015)
 
 #processing for home ownership
 table(data2015$RENTHOM1)
-data2015 = data2015[!is.na(data2015$RENTHOM1),]
+data2015 = data2015[!is.na(data2015$RENTHOM1),] #removing missing values
 data2015 = subset(data2015, data2015$RENTHOM1<7)
 table(data2015$RENTHOM1)
 nrow(data2015)
 
 #processing for diabetes
 table(data2015$DIABETE3)
-data2015 = data2015[!is.na(data2015$DIABETE3),]
+data2015 = data2015[!is.na(data2015$DIABETE3),] #removing missing values
 data2015 = subset(data2015, data2015$DIABETE3<2)
 table(data2015$DIABETE3)
 nrow(data2015)
 
 #processing for depression
 table(data2015$ADDEPEV2)
-data2015 = data2015[!is.na(data2015$ADDEPEV2),]
+data2015 = data2015[!is.na(data2015$ADDEPEV2),] #removing missing values
 data2015 = subset(data2015, data2015$ADDEPEV2<7)
 table(data2015$ADDEPEV2)
 nrow(data2015)
-
-#Final data
-table(data2015$X.AGEG5YR)
-table(data2015$DIABAGE2)
-table(data2015$SEX)
-table(data2015$X.RACE.G1)
-summary(data2015$X.BMI5)
-summary(data2015$MENTHLTH)
-table(data2015$EXERANY2)
-table(data2015$INCOME2)
-table(data2015$EMPLOY1)
-table(data2015$EDUCA)
-table(data2015$SCNTMNY1)
-table(data2015$SCNTMEL1)
-table(data2015$RENTHOM1)
-table(data2015$DIABETE3)
-table(data2015$ADDEPEV2)
