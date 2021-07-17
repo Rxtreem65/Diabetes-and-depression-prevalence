24e6b490dec8b5ec3bbd0558c2eed9284fe84987
Rxtreem65 <52712304+Rxtreem65@users.noreply.github.com>
2021-07-17 15:56:28 +0530

    Update SES_and_DD.R

--- SES_and_DD.R
+++ SES_and_DD.R
@@ -8,3 +8,30 @@ summary(total_data$SES)
 
 #binomial value for morbidity (0)absent (1)present
 total_data$MORB = 2 - total_data$ADDEPEV2 
+
+table(total_data$SES)
+table(total_data$ADDEPEV2)
+hist(total_data$SES)
+hist(total_data$SES,
+     xlab = "Social economic Status", 
+     ylab = "Percentage of Respondents", 
+     freq = FALSE, 
+     xlim = c(2,15),
+     main = "Variation of SES among Respondents"
+     )
+axis(side=1, at=seq(2,15, 1), labels=seq(2,15,1))
+
+hist(total_data$MENTHLTH,xlab = "Poor Mental Health Days", 
+     ylab = "Percentage of Respondents", 
+     main = "Variation of poor mental health among Respondents")
+
+hist(total_data$X.BMI5,xlab = "BMI", 
+     ylab = "Percentage of Respondents", 
+     main = "Variation of BMI among Respondents",
+     xlim = c(16,50),
+     freq = FALSE,
+     ylim = c(0,0.08)
+     )
+summary(total_data$X.BMI5)
+
+boxplot(total_data$SES)
