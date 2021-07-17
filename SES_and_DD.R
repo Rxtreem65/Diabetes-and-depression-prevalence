90699c84130e361f69c0c961a5dc3960d95c2f00
Rxtreem65 <52712304+Rxtreem65@users.noreply.github.com>
2021-07-17 15:58:32 +0530

    Update SES_and_DD.R

--- SES_and_DD.R
+++ SES_and_DD.R
@@ -2,8 +2,7 @@
 
 #SES variable
 #higher the value of SES better the social economic status
-total_data$SES = total_data$INCOME2+(1/total_data$EMPLOY1)+total_data$EDUCA
-              +total_data$SCNTMNY1+total_data$SCNTMEL1+(1/total_data$RENTHOM1)
+total_data$SES = total_data$INCOME2+(1/total_data$EMPLOY1)+total_data$EDUCA +total_data$SCNTMNY1+total_data$SCNTMEL1+(1/total_data$RENTHOM1)
 summary(total_data$SES)
 
 #binomial value for morbidity (0)absent (1)present
